85cbf0c96902781a67cdde99d9a76b35f02b8bed
KozukovMike <kozukovmisa@gmail.com>
2025-05-05 20:19:41 +0300

    added homework 3 task 1 and task 2

---
+++ HW3_2/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace HW3_2;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter first bool value");
+        var firstBoolValue = Convert.ToBoolean(Console.ReadLine());
+        Console.WriteLine("Enter second bool value");
+        var secondBoolValue = Convert.ToBoolean(Console.ReadLine());
+        Console.WriteLine("p\tq\tp&q\tp|q\tp^q\t!p");
+        Console.WriteLine($"{firstBoolValue}\t{secondBoolValue}\t{firstBoolValue && secondBoolValue}\t{firstBoolValue || secondBoolValue}\t{firstBoolValue ^ secondBoolValue}\t{!firstBoolValue}");
+    }
+}
