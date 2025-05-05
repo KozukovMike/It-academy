a322211eb8790af344f61daa8e1e8453e13f3178
KozukovMike <kozukovmisa@gmail.com>
2025-05-05 20:16:02 +0300

    added homework task 1 and task 2

---
+++ HW2/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace HW2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("what is your name?");
+            var userName = Console.ReadLine();
+            int userAge;
+            while (true)
+            {
+                Console.WriteLine("How old are you?");
+                userAge = Convert.ToInt32(Console.ReadLine());
+                if (userAge < 0)
+                {
+                    Console.WriteLine("You entered your age incorrectly\nTry one more time, please");
+                }
+                else
+                {
+                    break;
+                }
+            }
+            Console.WriteLine("What is your height?");
+            var userHeight = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"Hello, {userName}!\n\tYour age is {userAge}.\n\t\tYour height is {userHeight}");
+        }
+    }
+}
