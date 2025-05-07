72de809a492ce94345da397898750b4bee1f789d
KozukovMike <kozukovmisa@gmail.com>
2025-05-07 16:15:50 +0300

    Added homework 4 task 2

---
+++ HW4_2/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace HW4_2;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        for (var i = -95; i <= 95; i += 5)
+        {
+            if (i <= -10 || i >= 10)
+            {
+                Console.Write(i + " ");
+            } 
+        }
+    }
+}
