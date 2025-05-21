df910f6a2051e60126e1346a9adf213e453dc470
KozukovMike <kozukovmisa@gmail.com>
2025-05-21 21:51:54 +0300

    Added homework 6

---
+++ HW_6/Worker.cs
@@ -0,0 +1,42 @@
+﻿namespace HW_6;
+
+public class Worker : EmployeeBase
+{
+    private int _salary;
+
+    public string WorkArea { get; set; }
+
+    public int Salary
+    {
+        get { return _salary; }
+        set
+        {
+            if (value < 0)
+            {
+                Console.WriteLine("Enter a non-negative salary");
+                _salary = 0;
+            }
+            else
+            {
+                _salary = value;
+            }
+        }
+    }
+
+    public Worker() : base()
+    {
+        Salary = 0;
+        WorkArea = "Unknown";
+    }
+
+    public Worker(int salary, string workArea, string name, string position) : base(name, position)
+    {
+        Salary = salary;
+        WorkArea = workArea;
+    }
+
+    public override string GetDetails()
+    {
+        return $"{Name} has a salary of {Salary} working as a {WorkArea} at position {Position}";
+    }
+}
