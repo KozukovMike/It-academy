dd808e97f4a5f20acfc37ba1cfae9997151ca5c2
KozukovMike <kozukovmisa@gmail.com>
2025-06-02 15:11:20 +0300

    Added homework 7 task 1

---
+++ HW_7_1/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace HW_7_1;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int arrayLength = 6;
+        var array = new int[arrayLength];
+        var i = 0;
+        while (i < arrayLength)
+        {
+            Console.WriteLine("Enter an integer");
+            if (int.TryParse(Console.ReadLine(), out int number))
+            {
+                array[i] = number;
+                i++;
+            }
+            else
+            {
+                Console.WriteLine("Your number is not an integer");
+            }
+        }
+        Array.Sort(array);
+        Array.Reverse(array);
+        Console.WriteLine();
+        foreach (int item in array)
+        {
+            Console.Write(item + " ");
+        }
+    }
+}
