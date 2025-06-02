3d4aabf423b04c281f34dfb68588ea27144556b4
KozukovMike <kozukovmisa@gmail.com>
2025-06-02 16:38:38 +0300

    Added homework 8 task 1

---
+++ HW_8_1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.RegularExpressions;
+
+namespace HW_8_1;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter string");
+        var input = Console.ReadLine();
+        while (string.IsNullOrEmpty(input))
+        {
+            Console.WriteLine("Enter something");
+            input = Console.ReadLine();
+        }
+        var cleanInput = Regex.Replace(input.ToLower(), @"[\p{P}\p{Zs}]", "");
+        var reversedCleanInput = new string(cleanInput.Reverse().ToArray());
+        if (reversedCleanInput == cleanInput)
+        {
+            Console.WriteLine("Palindrome");
+        }
+        else
+        {
+            Console.WriteLine("Not a palindrome");
+        }
+    }
+}
