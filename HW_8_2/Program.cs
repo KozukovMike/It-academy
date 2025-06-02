7fa6b7358f3e57151a3117d29218b773dfb27e8d
KozukovMike <kozukovmisa@gmail.com>
2025-06-02 16:49:22 +0300

    Added homework 8 task 2

---
+++ HW_8_2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+
+namespace HW_8_2;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter string");
+        var input = Console.ReadLine();
+        var result = new StringBuilder();
+        foreach (char item in input)
+        {
+            if (item != ' ')
+            {
+                result.Append(item);
+            }
+        }
+        Console.WriteLine(result.ToString());
+    }
+}
