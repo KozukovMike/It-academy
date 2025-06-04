f252cc2dbf96a60892804982af4998c0e8167a11
KozukovMike <kozukovmisa@gmail.com>
2025-06-04 09:49:16 +0300

    Added homework 9

---
+++ HW_9_1/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace HW_9_1;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var studentList = new List<Student>()
+        {
+                new Student { Name = "Алексей", Age = 20, AverageRating = 8.5 },
+                new Student { Name = "Мария", Age = 18, AverageRating = 6.9 },
+                new Student { Name = "Иван", Age = 22, AverageRating = 7.8 },
+                new Student { Name = "Ольга", Age = 18, AverageRating = 6.9 }
+            };
+        studentList.Sort((student1, student2) => student2.AverageRating.CompareTo(student1.AverageRating));
+        var youngestStudent = studentList.First();
+        foreach (var student in studentList)
+        {
+            Console.WriteLine(student.Name + " " + student.AverageRating);
+            if (youngestStudent.Age > student.Age) // >=
+            {
+                youngestStudent = student;
+            }
+        }
+        Console.WriteLine("Youngest student");
+        Console.WriteLine(youngestStudent.Name + " " + youngestStudent.Age);
+    }
+}
