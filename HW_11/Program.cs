fda9e1a1c829dc78a8f329e61bd3f0a0a0bf6c78
KozukovMike <kozukovmisa@gmail.com>
2025-06-16 18:05:21 +0300

    Added homework 11

---
+++ HW_11/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace HW_11;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter your name");
+        var name = Console.ReadLine();
+        Console.WriteLine("Enter your age");
+        try
+        {
+            var age = int.Parse(Console.ReadLine());
+            try
+            {
+                var user = new User(name, age);
+            }
+            catch (CustomException e)
+            {
+                Console.WriteLine(e);
+            }
+            catch (CustomValueAgeException e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+        catch (FormatException e)
+        {
+            Console.Write(e.ToString());
+        }
+        
+    }
+}
---
+++ HW_11/User.cs
@@ -0,0 +1,21 @@
+﻿namespace HW_11;
+
+internal class User
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    public User(string name, int age)
+    {
+        if (name == "")
+        {
+            throw new CustomException("Your name must not be empty");
+        }
+        if (age < 0)
+        {
+            throw new CustomValueAgeException("Your age must be non-negative");
+        }
+        Name = name;
+        Age = age;
+    }
+}
