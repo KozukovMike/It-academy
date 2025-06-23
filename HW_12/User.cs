1e75258c4961b47b32bd7269af5a2ccea492f4ba
KozukovMike <kozukovmisa@gmail.com>
2025-06-23 15:42:14 +0300

    added homework 12

---
+++ HW_12/User.cs
@@ -0,0 +1,39 @@
+﻿namespace HW_12;
+
+public class User
+{
+    public string Name { get; set; }
+    private int _age;
+    public string Email { get; set; }
+
+    public int Age
+    {
+        get { return _age; }
+        set 
+        { 
+            if (value < 0)
+            {
+                Console.WriteLine("Age should be non-negative");
+                _age = 0;
+            }
+            else
+            {
+                _age = value;
+            }
+        }
+    }
+
+    public User()
+    {
+        Name = string.Empty;
+        Age = 0;
+        Email = string.Empty;
+    }
+
+    public User(string name, int age, string email)
+    {
+        Name = name;
+        Age = age;
+        Email = email;
+    }
+}
---
+++ HW_12/WorkWithUser.cs
@@ -0,0 +1,57 @@
+﻿using System.Text.Json;
+
+namespace HW_12;
+
+public static class WorkWithUser
+{
+    public static List<User> UsersList = new List<User>();
+
+    private static string FilePath = "users.json";
+
+    public static void AddUser(User user)
+    {
+        UsersList.Add(user);
+    }
+
+    public static void ShowUsers()
+    {
+        Console.WriteLine();
+        foreach (User user in UsersList)
+        {
+            Console.WriteLine($"Name: {user.Name}, Age: {user.Age}, Email: {user.Email}");
+        }
+        Console.WriteLine();
+    }
+
+    public static void CreateFile()
+    {
+        if (!File.Exists(FilePath))
+        {
+            using (File.Create(FilePath)) { };
+        }
+    }
+
+    public static void SaveUsersToFile()
+    {
+        string json = JsonSerializer.Serialize(UsersList);
+        File.WriteAllText(FilePath, json);
+        UsersList.Clear();
+    }
+
+    public static void LoadUsersToList()
+    {
+        string json = File.ReadAllText(FilePath);
+        try
+        {
+            UsersList = JsonSerializer.Deserialize<List<User>>(json).Concat(UsersList).ToList();
+        }
+        catch (System.Text.Json.JsonException e)
+        {
+        }
+    }
+
+    public static void ClearFile()
+    {
+        File.WriteAllText(FilePath, string.Empty);
+    }
+}
