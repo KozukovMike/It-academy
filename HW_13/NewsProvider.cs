2dfc407891d3cecb7995471116c0c20cd9943bd9
KozukovMike <kozukovmisa@gmail.com>
2025-06-25 16:19:08 +0300

    added homework 13

---
+++ HW_13/NewsProvider.cs
@@ -0,0 +1,16 @@
+﻿namespace HW_13;
+
+public class NewsProvider
+{
+    public event EventHandler<NewsEventArgs> NewsEvent;
+
+    public void SendNews(string category, string news)
+    {
+        OnNewsEvent(new NewsEventArgs(category, news));
+    }
+
+    protected void OnNewsEvent(NewsEventArgs e)
+    {
+        NewsEvent?.Invoke(this, e);
+    }
+}
